39cd3f4fe510e423444ef90c22b7478359cf437e
Marc Love <marcslove@gmail.com>
2010-04-02 07:55:48 -0700

    Missed some fixes from switch to Carrierwave.

--- app/uploaders/site_asset_uploader.rb
+++ app/uploaders/site_asset_uploader.rb
@@ -24,19 +24,19 @@ class SiteAssetUploader < CarrierWave::Uploader::Base
   end
   
   version :small do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_limit => [100, 100]
   end
   
   version :medium do
-    process :resize_to_fit => [240, 240]
+    process :resize_to_limit => [240, 240]
   end
   
   version :large do
-    process :resize_to_fit => [480, 480]
+    process :resize_to_limit => [480, 480]
   end
   
   version :xlarge do
-    process :resize_to_fit => [600, 600]
+    process :resize_to_limit => [600, 600]
   end
   
   # Add a white list of extensions which are allowed to be uploaded,
@@ -47,10 +47,7 @@ class SiteAssetUploader < CarrierWave::Uploader::Base
   
   private
     def full_filename(for_file)
-      [super(for_file), version_name].compact.join('_')
-    end
-
-    def full_original_filename
-      [super, version_name].compact.join('_')
+      fn, ext = for_file.split('.')[0..-2].join('.'), for_file.split('.')[-1..-1]
+      [[fn, version_name].compact.join('_'), ext].join('.')
     end
 end
