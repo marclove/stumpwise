2d345575d0a5bb0758356e746e87163ab27521a4
Marc Love <marcslove@gmail.com>
2010-04-03 12:04:00 -0700

    Update to application.js for prev commit.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -44,12 +44,11 @@ $(document).ready(function() {
 	// Sortable Data Table
 	$("#supporters_table").tablesorter({
 		headers:{
-			3:{sorter:false},
-			6:{sorter:false}
+			5:{sorter:false}
 		},
 		widgets: ['zebra'],
 		widthFixed: true,
-		sortList:[[7,1]]
+		sortList:[[6,1]]
 	}).tablesorterPager({container:$("#pager"), size:20});
 
 	// Sortable Data Table Clickable Rows
