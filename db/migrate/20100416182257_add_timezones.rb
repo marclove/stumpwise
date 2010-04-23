f12456a8dcf97111708bd53447f1b1234bdaa97c
Marc Love <marcslove@gmail.com>
2010-04-23 03:55:39 -0700

    Missed file for timezone commit.

--- db/migrate/20100416182257_add_timezones.rb
+++ db/migrate/20100416182257_add_timezones.rb
@@ -1,11 +1,11 @@
 class AddTimezones < ActiveRecord::Migration
   def self.up
-    add_column :sites, :timezone, :string, :default => "Pacific Time (US & Canada)"
-    add_column :users, :timezone, :string, :default => "Pacific Time (US & Canada)"
+    add_column :sites, :time_zone, :string, :default => "Pacific Time (US & Canada)"
+    add_column :users, :time_zone, :string, :default => "Pacific Time (US & Canada)"
   end
 
   def self.down
-    remove_column :sites, :timezone, :string
-    remove_column :sites, :timezone, :string
+    remove_column :sites, :time_zone
+    remove_column :users, :time_zone
   end
 end
