bda19d36b9eee75855ffec0676870ac9286197f2
Marc Love <marcslove@gmail.com>
2010-05-10 16:02:47 -0700

    Update Blog tests. * Validate articles relationship * Require template names * Validate defaults for template names

--- test/unit/blog_test.rb
+++ test/unit/blog_test.rb
@@ -1,51 +1,27 @@
 require 'test_helper'
 
 class BlogTest < ActiveSupport::TestCase
-  context "Blog:" do
-    setup do
-      @theme = Theme.create(:name => "theme")
-      @site = Site.create(:subdomain => "my_site", :theme => @theme)
+  context "A blog" do
+    should_have_many :articles, :dependent => :destroy
+    should_validate_presence_of :template_name
+    should_validate_presence_of :article_template_name
+    
+    should "know its liquid name" do
+      assert_equal "blog", Blog.new.liquid_name
     end
-  
-    context "An instance of a Blog" do
-      setup do
-        @valid_attributes = {
-          :title => "Test Blog Title",
-          :theme_id => @theme.id,
-          :site_id => @site.id
-        }
-        def new_blog(options={})
-          Blog.new(@valid_attributes.merge(options))
-        end
-      end
-      
-      should "save with valid attributes" do
-        assert_difference 'Blog.count' do
-          blog = new_blog
-          blog.save
-          assert blog.errors.blank?
-        end
-      end
-      
-      should "require a template name" do
-        assert_no_difference 'Blog.count' do
-          blog = new_blog(:template_name => '')
-          blog.save
-          assert blog.errors.on(:template_name)
-        end
-      end
-      
-      should "require an article template name" do
-        assert_no_difference 'Blog.count' do
-          blog = new_blog(:article_template_name => '')
-          blog.save
-          assert blog.errors.on(:article_template_name)
-        end
-      end
-      
-      should "have many articles" do
-        assert new_blog.respond_to?(:articles)
-      end
+    
+    should "have a default template name of blog.tpl on initialize" do
+      assert_equal "blog.tpl", Blog.new.template_name
     end
-  end
+    
+    should "have a default article template name of article.tpl on initialize" do
+      assert_equal "article.tpl", Blog.new.article_template_name
+    end
+
+    should "be able to be transformed into a liquid drop" do
+      assert_instance_of BlogDrop, Blog.new.to_liquid
+    end
+    
+    should_eventually "return its template"
+  end  
 end
