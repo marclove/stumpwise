480cbb04d3d3277663a89802b7f0fe8016836367
Marc Love <marcslove@gmail.com>
2010-05-10 17:14:43 -0700

    Add Admin::ContributionsController tests

--- test/functional/admin/contributions_controller_test.rb
+++ test/functional/admin/contributions_controller_test.rb
@@ -1,8 +1,137 @@
 require 'test_helper'
 
 class Admin::ContributionsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "with administrator logged in" do
+    setup do
+      login_as(:admin)
+      on_site(:with_content)
+    end
+    
+    context "on GET to :index" do
+      setup { get :index }
+      should_render_with :application, :index
+      should_not_set_the_flash
+      should_paginate :contributions
+      should_assign_to(:contribution_total){ 6000 }
+    end
+    
+    should_eventually "on GET to :search"
+    
+    context "on GET to :show" do
+      setup { get :show, :id => contributions(:with_content_approved_1) }
+      should_render_with :application, :show
+      should_not_set_the_flash
+      should_assign_to :contribution
+    end
+    
+    should "raise an error when attempting to :show a contribution belonging to other site" do
+      assert_raise ActiveRecord::RecordNotFound do
+        get :show, :id => 1
+      end
+    end
+    
+    context "on PUT to :refund that's successful" do
+      setup do
+        response = mock()
+        response.expects(:success?).returns(true).once
+        Contribution.any_instance.expects(:refund).returns(response)
+        put :refund, :id => contributions(:with_content_approved_1)
+      end
+      
+      should_redirect_to("contribution show page"){ admin_contribution_path(contributions(:with_content_approved_1))}
+      should_set_the_flash_to I18n.t('contribution.refund.success')
+    end
+    
+    context "on PUT to :refund that fails" do
+      setup do
+        response = mock()
+        response.expects(:success?).returns(false).once
+        response.expects(:message).returns("It failed").once
+        Contribution.any_instance.expects(:refund).returns(response)
+        put :refund, :id => contributions(:with_content_approved_1)
+      end
+      
+      should_redirect_to("contribution show page"){ admin_contribution_path(contributions(:with_content_approved_1))}
+      should_set_the_flash_to "#{I18n.t('contribution.refund.rejected')} It failed"
+    end
+  end
+
+
+  context "with authorized user but non-administrator logged in" do
+    setup do
+      login_as(:authorized)
+      on_site(:with_content)
+    end
+    
+    context "on GET to :index" do
+      setup { get :index }
+      should_render_with :application, :index
+      should_not_set_the_flash
+      should_paginate :contributions
+      should_assign_to(:contribution_total){ 6000 }
+    end
+    
+    should_eventually "on GET to :search"
+    
+    context "on GET to :show" do
+      setup { get :show, :id => contributions(:with_content_approved_1) }
+      should_render_with :application, :show
+      should_not_set_the_flash
+      should_assign_to :contribution
+    end
+    
+    should "raise an error when attempting to :show a contribution belonging to other site" do
+      assert_raise ActiveRecord::RecordNotFound do
+        get :show, :id => 1
+      end
+    end
+    
+    context "on PUT to :refund that's successful" do
+      setup do
+        response = mock()
+        response.expects(:success?).returns(true).once
+        Contribution.any_instance.expects(:refund).returns(response)
+        put :refund, :id => contributions(:with_content_approved_1)
+      end
+      
+      should_redirect_to("contribution show page"){ admin_contribution_path(contributions(:with_content_approved_1))}
+      should_set_the_flash_to I18n.t('contribution.refund.success')
+    end
+    
+    context "on PUT to :refund that fails" do
+      setup do
+        response = mock()
+        response.expects(:success?).returns(false).once
+        response.expects(:message).returns("It failed").once
+        Contribution.any_instance.expects(:refund).returns(response)
+        put :refund, :id => contributions(:with_content_approved_1)
+      end
+      
+      should_redirect_to("contribution show page"){ admin_contribution_path(contributions(:with_content_approved_1))}
+      should_set_the_flash_to "#{I18n.t('contribution.refund.rejected')} It failed"
+    end
+  end
+
+
+  context "with unauthorized user logged in" do
+    setup do
+      login_as(:unauthorized)
+      on_site(:with_content)
+    end
+
+    context "on GET to :index" do
+      setup { get :index }
+      should_redirect_to("super admin login"){ admin_login_path }
+    end
+
+    context "on GET to :show" do
+      setup { get :show, :id => 1 }
+      should_redirect_to("super admin login"){ admin_login_path }
+    end
+
+    context "on PUT to :refund" do
+      setup { put :refund, :id => 1 }
+      should_redirect_to("super admin login"){ admin_login_path }
+    end
   end
 end
