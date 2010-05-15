c1702268ce7f3e0b70637630abcaae8c6ea12213
Marc Love <marcslove@gmail.com>
2010-05-15 04:17:03 -0700

    Update Admin::SupportersController#export to use the Supportership#created_at join date

--- app/controllers/admin/supporters_controller.rb
+++ app/controllers/admin/supporters_controller.rb
@@ -13,18 +13,32 @@ class Admin::SupportersController < ApplicationController
   end
   
   def export
-    @supporters = current_site.supporters.all(:order => 'created_at asc')
+    @supporterships = current_site.supporterships.find(:all, :order => 'created_at asc', :include => [:supporter])
     report = StringIO.new
     CSV::Writer.generate(report,',') do |r|
-      r << ['Name Prefix','First Name','Last Name','Name Suffix','Email','Phone','Street Address','City','County','State','Zip Code','Country','Join Time']
-      @supporters.each do |s|
-        r << [s.name_prefix,s.first_name,s.last_name,s.name_suffix,s.email,s.phone,s.thoroughfare,s.locality,s.subadministrative_area,s.administrative_area,s.postal_code,s.country,s.created_at]
+      #r << ['Name Prefix','First Name','Last Name','Name Suffix','Email','Phone','Street Address','City','County','State','Zip Code','Country','Join Time']
+      r << ['Email','Zip Code','Join Time']
+      @supporterships.each do |s|
+        r << [
+          #s.supporter.name_prefix,
+          #s.supporter.first_name,
+          #s.supporter.last_name,
+          #s.supporter.name_suffix,
+          s.supporter.email,
+          #s.supporter.phone,
+          #s.supporter.thoroughfare,
+          #s.supporter.locality,
+          #s.supporter.subadministrative_area,
+          #s.supporter.administrative_area,
+          s.supporter.postal_code,
+          #s.supporter.country,
+          s.created_at.strftime('%Y-%m-%d %I:%M %p')]
       end
     end
     report.rewind
     respond_to do |format|
       format.csv { 
-        send_data report.string, :type => "text/csv", :filename => "Supporters - #{Time.now.strftime('%Y-%m-%d-%H:%M')}.csv"
+        send_data report.string, :type => "text/csv", :filename => "Supporters - #{Time.now.strftime('%Y-%m-%d-%H%M')}.csv"
       }
     end
   end
