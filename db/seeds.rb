eb3ce2e14a64416cc03063030bc3b9755bfdf528
Marc Love <marcslove@gmail.com>
2010-05-16 14:12:34 -0700

    Updating database seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'faker'
 
+# Themes / Layouts / Theme Assets
+ThemeAsset.delete_all
 default_theme = Theme.create(:name => "Default Theme")
 default_theme.layouts.create(
   :filename => "layout.tpl",
@@ -17,25 +19,8 @@ default_theme.templates.create(
   :filename => 'article.tpl',
   :content => File.new("#{RAILS_ROOT}/db/seeds/article.tpl").readlines.join
 )
-
-=begin
-default_theme.assets.create(:uploaded_data => ActionController::TestUploadedFile.new("#{RAILS_ROOT}/db/seeds/master.css", "text/css"))
-default_theme.assets.create(:uploaded_data => ActionController::TestUploadedFile.new("#{RAILS_ROOT}/db/seeds/grid.css", "text/css"))
-
-default_theme.assets.create(:uploaded_data => ActionController::TestUploadedFile.new("#{RAILS_ROOT}/db/seeds/bg.jpg", "image/jpeg"))
-default_theme.assets.create(:uploaded_data => ActionController::TestUploadedFile.new("#{RAILS_ROOT}/db/seeds/footer_bg.gif", "image/gif"))
-default_theme.assets.create(:uploaded_data => ActionController::TestUploadedFile.new("#{RAILS_ROOT}/db/seeds/profile_drop_shadow_top.png", "image/png"))
-default_theme.assets.create(:uploaded_data => ActionController::TestUploadedFile.new("#{RAILS_ROOT}/db/seeds/profile_drop_shadow.png", "image/png"))
-default_theme.assets.create(:uploaded_data => ActionController::TestUploadedFile.new("#{RAILS_ROOT}/db/seeds/facebook_32.png", "image/png"))
-default_theme.assets.create(:uploaded_data => ActionController::TestUploadedFile.new("#{RAILS_ROOT}/db/seeds/flickr_32.png", "image/png"))
-default_theme.assets.create(:uploaded_data => ActionController::TestUploadedFile.new("#{RAILS_ROOT}/db/seeds/linkedin_32.png", "image/png"))
-default_theme.assets.create(:uploaded_data => ActionController::TestUploadedFile.new("#{RAILS_ROOT}/db/seeds/twitter_32.png", "image/png"))
-default_theme.assets.create(:uploaded_data => ActionController::TestUploadedFile.new("#{RAILS_ROOT}/db/seeds/youtube_32.png", "image/png"))
-=end
-
 default_theme.assets.create(:file => File.open("#{RAILS_ROOT}/db/seeds/master.css"))
 default_theme.assets.create(:file => File.open("#{RAILS_ROOT}/db/seeds/grid.css"))
-
 default_theme.assets.create(:file => File.open("#{RAILS_ROOT}/db/seeds/bg.jpg"))
 default_theme.assets.create(:file => File.open("#{RAILS_ROOT}/db/seeds/footer_bg.gif"))
 default_theme.assets.create(:file => File.open("#{RAILS_ROOT}/db/seeds/profile_drop_shadow_top.png"))
@@ -47,55 +32,69 @@ default_theme.assets.create(:file => File.open("#{RAILS_ROOT}/db/seeds/twitter_3
 default_theme.assets.create(:file => File.open("#{RAILS_ROOT}/db/seeds/youtube_32.png"))
 
 
-administrator =
-  User.create(
-    :first_name => "Marc",
-    :last_name => "Love",
-    :email => "marc.love@progressbound.com",
-    :password => "marclove",
-    :password_confirmation => "marclove"
-  )
+# User Accounts
+administrator = User.create(
+  :first_name => "Marc",
+  :last_name => "Love",
+  :email => "marc.love@progressbound.com",
+  :password => "marclove",
+  :password_confirmation => "marclove",
+  :time_zone => "Pacific Time (US & Canada)",
+  :accepted_campaign_terms => true
+)
 administrator.update_attribute(:super_admin, true)
+candidate = User.create(
+  :first_name => "Anthony",
+  :last_name => "Woods",
+  :email => "tony@anthonywoods.com",
+  :password => "test1234",
+  :password_confirmation => "test1234",
+  :time_zone => "Pacific Time (US & Canada)",
+  :accepted_campaign_terms => true
+)
 
-candidate = 
-  User.create(
-    :first_name => "Evan",
-    :last_name => "Low",
-    :email => "evan@evanlow-local.com",
-    :password => "trial198",
-    :password_confirmation => "trial198"
-  )
 
+# Sites
 site = candidate.owned_sites.create(
-    :subdomain => "evanlow",
-    :custom_domain => "evanlow-local.com",
-    :name => "Mayor Evan Low",
-    :subhead => "Campbell City Council",
-    :description => "",
-    :keywords => "",
-    :disclaimer => "Paid for and maintained by THE COMMITTEE TO RE-ELECT EVAN LOW",
-    :facebook_page_id => "81588820822",
-    :public_email => "evan@evanlow-local.com"
-  )
-site.update_attribute(:theme, default_theme)
-site.update_attribute(:candidate_photo, File.open("#{RAILS_ROOT}/db/seeds/evan.jpg"))
+  :subdomain => "woods",
+  :name => "Anthony Woods",
+  :subhead => "The Courage of Conviction",
+  :description => "Iraq War veteran running for California's 10th Congressional District",
+  :keywords => "Iraq War, Veterans, CA-10, Congress",
+  :disclaimer => "Paid for and maintained by Anthony Woods For Congress",
+  :facebook_page_id => "81588820822",
+  :twitter_username => "woods4congress",
+  :google_analytics_id => "UA-9999999-1",
+  :candidate_photo => File.open("#{RAILS_ROOT}/db/seeds/anthonywoods.jpg"),
+  :theme_id => 1,
+  :eligibility_statement => "I am a United States citizen.",
+  :campaign_legal_name => "Anthony Woods For Congress",
+  :campaign_street => "2400 Market Street",
+  :campaign_city => "San Francisco",
+  :campaign_state => "CA",
+  :campaign_zip => "94114",
+  :campaign_email => "info@localdev.com",
+  :campaign_phone => "415-555-1234",
+  :time_zone => "Pacific Time (US & Canada)"
+)
 
-Administratorship.create(:site => site, :administrator => candidate)
+# Permissions
+Administratorship.create(:site_id => 1, :administrator_id => 2)
 
+
+# Site Content
 blog = Blog.create(
   :site => site,
   :title => "News", 
   :slug => 'news', 
-  :template_name => 'blog.tpl', 
-  :article_template_name => "article.tpl",
   :published => true,
   :show_in_navigation => true
 )
 
-10.times do |t|
+8.times do |t|
   Article.create(
-    :site_id => site.id,
-    :parent_id => blog.id,
+    :site => site,
+    :parent => blog,
     :title => Faker::Lorem.words.join(" ").titleize,
     :body => "<p>" + Faker::Lorem.paragraphs(5).join("</p><p>") + "</p>",
     :created_at => Time.now - (10-t).days,
@@ -104,10 +103,39 @@ blog = Blog.create(
 end
 
 Page.create(
-  :site_id => site.id,
-  :title => 'About Evan',
-  :template_name => 'page.tpl',
-  :body => File.new("#{RAILS_ROOT}/db/seeds/about-evan.html").readlines.join,
+  :site => site,
+  :title => 'About Anthony',
+  :body => File.new("#{RAILS_ROOT}/db/seeds/about.html").readlines.join,
   :published => true,
   :show_in_navigation => true
 )
+
+issues = Page.create(
+  :site => site,
+  :title => 'Issues',
+  :body => "My issues are",
+  :published => true,
+  :show_in_navigation => true
+)
+
+Page.create(
+  :site => site,
+  :parent => issues,
+  :title => 'Health Care',
+  :body => "My position on health care",
+  :published => true,
+  :show_in_navigation => true
+)
+
+Page.create(
+  :site => site,
+  :parent => issues,
+  :title => 'Veterans',
+  :body => "My position on veterans",
+  :published => true,
+  :show_in_navigation => true
+)
+
+# Contributions & Transactions
+
+# Supporters
