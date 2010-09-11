eeaf44fcf27318d403f873e513608eae0a0b25cc
Marc Love <marcslove@gmail.com>
2010-09-10 19:34:26 -0700

    Fix/hack contribution settlement delayed job to work with DJ bug

--- app/jobs/settlement_check_job.rb
+++ app/jobs/settlement_check_job.rb
@@ -1,7 +1,12 @@
 class SettlementCheckJob < Struct.new(:contribution_id)
   def perform
     @contribution = Contribution.find(contribution_id)
-    unless @contribution.settle!
+    transaction = Braintree::Transaction.find(@contribution.transaction_id)
+    
+    if transaction.status == 'settled'
+      @contribution.increment(:net_amount, amount)            # add_amount_to_net
+      @contribution.update_attributes(:status => 'settled')   # mark as settled
+    else
       raise Stumpwise::Transaction::NotSettledError
     end
   end
