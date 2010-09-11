2ead5cda45686a2c86dc51278c78a0716871e65e
Marc Love <marcslove@gmail.com>
2010-09-10 20:04:47 -0700

    Yet another attempt to hack DelayedJob into working

--- app/jobs/settlement_check_job.rb
+++ app/jobs/settlement_check_job.rb
@@ -4,8 +4,9 @@ class SettlementCheckJob < Struct.new(:contribution_id)
     transaction = Braintree::Transaction.find(@contribution.transaction_id)
     
     if transaction.status == 'settled'
-      @contribution.increment(:net_amount, amount)            # add_amount_to_net
-      @contribution.update_attributes(:status => 'settled')   # mark as settled
+      @contribution.increment(:net_amount, @contribution.amount)  # add_amount_to_net
+      @contribution.status = 'settled'                            # mark as settled
+      @contribution.save!
     else
       raise Stumpwise::Transaction::NotSettledError
     end
