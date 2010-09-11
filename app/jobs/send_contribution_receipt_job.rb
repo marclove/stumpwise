2bae2c4be3336b2ca41d8e33443f4e38daffd2d2
Marc Love <marcslove@gmail.com>
2010-09-10 23:10:12 -0700

    Wow, um yea.

--- app/jobs/send_contribution_receipt_job.rb
+++ app/jobs/send_contribution_receipt_job.rb
@@ -1,3 +1,5 @@
 class SendContributionReceiptJob < Struct.new(:contribution_id)
-  ContributionNotifier.deliver_send_receipt(Contribution.find(contribution_id))
+  def perform
+    ContributionNotifier.deliver_send_receipt(Contribution.find(contribution_id))
+  end
 end
