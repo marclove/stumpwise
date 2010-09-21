7ba80b58f538b23b68dc46534217f0a4731c0964
Marc Love <marcslove@gmail.com>
2010-09-20 20:50:01 -0700

    [FIX] SettlementCheckJob shouldn't "settle" a transaction that's been voided/refunded

--- app/jobs/settlement_check_job.rb
+++ app/jobs/settlement_check_job.rb
@@ -3,12 +3,14 @@ class SettlementCheckJob < Struct.new(:contribution_id)
     @contribution = Contribution.find(contribution_id)
     transaction = Braintree::Transaction.find(@contribution.transaction_id)
     
-    if transaction.status == 'settled'
-      @contribution.increment(:net_amount, @contribution.amount)  # add_amount_to_net
-      @contribution.status = 'settled'                            # mark as settled
-      @contribution.save!
-    else
-      raise Stumpwise::Transaction::NotSettledError
+    if @contribution.approved? # don't do anything if it's been voided or refunded
+      if transaction.status == 'settled'
+        @contribution.increment(:net_amount, @contribution.amount)  # add_amount_to_net
+        @contribution.status = 'settled'                            # mark as settled
+        @contribution.save!
+      else
+        raise Stumpwise::Transaction::NotSettledError
+      end
     end
   end
 end
