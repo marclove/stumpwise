fca0163ebd1b9800c696c6abb0fb402e1197d71e
Marc Love <marcslove@gmail.com>
2010-10-04 21:37:24 -0700

    Validation check update

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,4 +1,4 @@
 RegEmailName   = '[\w\.%\+\-]+'
 RegDomainHead  = '(?:[A-Z0-9\-]+\.)+'
-RegDomainTLD   = '(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)'
+RegDomainTLD   = '(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|museum)'
 RegEmailOk     = /\A#{RegEmailName}@#{RegDomainHead}#{RegDomainTLD}\z/i
