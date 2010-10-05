5af53f4827524b94501f122ae771ef71127f893b
Marc Love <marcslove@gmail.com>
2010-10-04 22:32:00 -0700

    Expand email validator regex

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,4 +1,4 @@
 RegEmailName   = '[\w\.%\+\-]+'
 RegDomainHead  = '(?:[A-Z0-9\-]+\.)+'
-RegDomainTLD   = '(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|museum)'
+RegDomainTLD   = '(?:[A-Z]{2}|biz|com|info|name|net|org|pro|aero|asia|cat|coop|edu|gov|int|jobs|mil|mobi|museum|tel|travel|arpa|nato)'
 RegEmailOk     = /\A#{RegEmailName}@#{RegDomainHead}#{RegDomainTLD}\z/i
