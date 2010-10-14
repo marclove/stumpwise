810468851ef53c82101e5177980d21b6a92a178a
Marc Love <marcslove@gmail.com>
2010-10-13 20:51:13 -0700

    Supporters export now includes all supporter information.

--- app/controllers/admin/supporters_controller.rb
+++ app/controllers/admin/supporters_controller.rb
@@ -14,22 +14,21 @@ class Admin::SupportersController < AdminController
     @supporterships = current_site.supporterships.find(:all, :order => 'created_at asc', :include => [:supporter])
     report = StringIO.new
     CSV::Writer.generate(report,',') do |r|
-      #r << ['Name Prefix','First Name','Last Name','Name Suffix','Email','Phone','Street Address','City','County','State','Zip Code','Country','Join Time']
-      r << ['Email','Zip Code','Join Time']
+      r << ['Name Prefix','First Name','Last Name','Name Suffix','Email','Phone','Street Address','City','County','State','Zip Code','Country','Join Time']
       @supporterships.each do |s|
         r << [
-          #s.supporter.name_prefix,
-          #s.supporter.first_name,
-          #s.supporter.last_name,
-          #s.supporter.name_suffix,
+          s.supporter.name_prefix,
+          s.supporter.first_name,
+          s.supporter.last_name,
+          s.supporter.name_suffix,
           s.supporter.email,
-          #s.supporter.phone,
-          #s.supporter.thoroughfare,
-          #s.supporter.locality,
-          #s.supporter.subadministrative_area,
-          #s.supporter.administrative_area,
+          s.supporter.phone,
+          s.supporter.thoroughfare,
+          s.supporter.locality,
+          s.supporter.subadministrative_area,
+          s.supporter.administrative_area,
           s.supporter.postal_code,
-          #s.supporter.country,
+          s.supporter.country,
           s.created_at.strftime('%Y-%m-%d %I:%M %p')]
       end
     end
