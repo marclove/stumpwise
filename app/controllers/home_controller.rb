a4bad74bc07f87d60dc9d37934c60b1b24a7a9e9
Marc Love <marcslove@gmail.com>
2011-01-24 17:02:42 -0800

    Remove credit card entry from signup process.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,33 +20,19 @@ class HomeController < ApplicationController
     site = params[:site].merge(:active => true)
     @site = Site.new(params[:site])
     
-    @customer_result = Braintree::Customer.create(
-      :credit_card => params[:credit_card].update(:options => {:verify_card => true})
-    )
-    
-    if @customer_result.success?
-      @cc = @customer_result.customer.credit_cards[0]
-      site[:credit_card_token]          = @cc.token
-      site[:credit_card_expiration]     = Time.utc(@cc.expiration_year, @cc.expiration_month).end_of_month
-      
-      User.transaction do
-        Site.transaction do
-          Administratorship.transaction do
-            @user.save!
-            @site = @user.owned_sites.build(site)
-            @site.save!
-            Administratorship.create!(:site => @site, :administrator => @user)
-          end
+    User.transaction do
+      Site.transaction do
+        Administratorship.transaction do
+          @user.save!
+          @site = @user.owned_sites.build(site)
+          @site.save!
+          Administratorship.create!(:site => @site, :administrator => @user)
         end
       end
-      @site.set_theme!(Theme.first.id.to_s)
-      UserSession.create(@user)
-      redirect_to "http://#{@site.subdomain}.#{HOST}/admin/site/edit"
-    else
-      flash.now[:error] = t("site.create.invalid_card")
-      render :action => 'signup'
     end
-    
+    @site.set_theme!(Theme.first.id.to_s)
+    UserSession.create(@user)
+    redirect_to "http://#{@site.subdomain}.#{HOST}/admin/site/edit"
   rescue ActiveRecord::RecordInvalid => e
     notify_hoptoad(e)
     flash.now[:error] = t("site.create.fail")
