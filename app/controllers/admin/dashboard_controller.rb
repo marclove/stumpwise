eb07071c997713c07a53c520031187accd6e6c6c
Marc Love <marcslove@gmail.com>
2011-03-17 03:22:22 -0700

    Forgot to make DashboardController a subclass of AdminController

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,4 @@
-class Admin::DashboardController < ApplicationController
+class Admin::DashboardController < AdminController
   def index
     @progress_tracker = current_site.progress_tracker
   end
